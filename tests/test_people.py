341234aa7879516ed040abee9f1ff3562a919077
Guillermo Veneranda <8316152+gville@users.noreply.github.com>
2021-09-21 10:15:23 -0300

    Add error schema tests

--- tests/test_people.py
+++ tests/test_people.py
@@ -41,10 +41,12 @@ class TestPeopleApi:
 
     def test_get_person_by_id(self):
         response = self.client.get_person(1)
+        
         assert_that(response.status_code).is_equal_to(200)
         person = Person(**response.json())
         assert_that(person.fname).is_equal_to('Doug')
         assert_that(person.lname).is_equal_to('Farrell')
+        assert_that(person.person_id).is_equal_to(1)
     
     def test_created_person_can_be_deleted(self):
         payload, _, lname = self.client.generate_new_person_data()
@@ -56,6 +58,15 @@ class TestPeopleApi:
         
         response = self.client.delete_person(new_person['person_id'])
         assert_that(response.status_code).is_equal_to(200)
+
+    def test_unexistent_person_can_not_be_deleted(self):
+        unexistent_person_id = 0
+        response = self.client.delete_person(unexistent_person_id)
+        
+        assert_that(response.status_code).is_equal_to(404)
+        error = Error(**response.json())
+        assert_that(error.status).is_equal_to(404)
+        assert_that(error.detail).is_equal_to(f'Person not found for id {unexistent_person_id}')
 
     def test_created_person_can_be_updated(self):
         payload, _, lname = self.client.generate_new_person_data()
@@ -72,16 +83,30 @@ class TestPeopleApi:
 
     def test_person_schema(self):
         schema = data_reader.read_json_from_file("person_schema.json")
-        response = self.client.get_person(1)
         validator = Validator(schema, required_all=True)
+        
+        response = self.client.get_person(1)
+        
         is_valid = validator.validate(response.json())
         assert_that(is_valid, description=validator.errors).is_true()
 
     def test_people_schema(self):
         schema = data_reader.read_json_from_file("person_schema.json")
-        response = self.client.get_people()
         validator = Validator(schema, required_all=True)
+        
+        response = self.client.get_people()
+        
         with soft_assertions():
             for person in response.json():
                 is_valid = validator.validate(person)
                 assert_that(is_valid, description=validator.errors).is_true()
+
+    def test_error_schema(self):
+        schema = data_reader.read_json_from_file('error_schema.json')
+        validator = Validator(schema)
+        
+        unexistent_person_id = 0
+        response = self.client.delete_person(unexistent_person_id)
+        
+        is_valid = validator.validate(response.json())
+        assert_that(is_valid, description=validator.errors).is_true()
